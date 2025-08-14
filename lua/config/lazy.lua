30a808f4d3c5fa35aebbc6a71a3c52e7d8241d6e
lapLapych <xarashocoba@gmail.com>
2025-08-14 10:56:49 +0300

    New color theme
    gruvbox

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,9 +28,9 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = {  "catppuccin", "tokyonight", "habamax" } },
+  install = { colorscheme = {  "catppuccin", "tokyonight", "gruvbox", "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
 
-vim.cmd[[colorscheme catppuccin]]
+vim.cmd[[colorscheme gruvbox]]
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,7 +7,7 @@ return {
   config = function()
     require("lualine").setup({
       options = {
-        theme = "catppuccin",
+        theme = "gruvbox",
         section_separators = { left = "", right = "" },
         component_separators = { left = "", right = "" },
         globalstatus = true,

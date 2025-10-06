1d3475d3300cf06ba15f20cb957208c38e45110c
lapLapych <xarashocoba@gmail.com>
2025-10-06 09:58:04 +0300

    move from csharp-ls to roslyn

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,17 +9,17 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "csharp_ls", "clangd", "pyright" },
+				ensure_installed = { "lua_ls", "clangd", "pyright" },
 			})
 		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-	        vim.lsp.enable('lua_ls')
-            vim.lsp.enable('csharp_ls')
-            vim.lsp.enable('clangd')
-            vim.lsp.enable('pyright')
+			vim.lsp.enable("lua_ls")
+			vim.lsp.enable("clangd")
+			vim.lsp.enable("pyright")
+			vim.lsp.enable("roslyn")
 
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
